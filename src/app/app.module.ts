8c3a665f96166c391a3074af126c9af08a5b3edc
gregoryalbertt <gregoryalbertt@gmail.com>
2017-03-26 04:09:49 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyBmRa5YJSABIr5hpgfu9bzEHRr2og2CRG4",
+  authDomain: "af2-lists-b3c86.firebaseapp.com",
+  databaseURL: "https://af2-lists-b3c86.firebaseio.com",
+  storageBucket: "af2-lists-b3c86.appspot.com",
+  messagingSenderId: "401884397030"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}

71ebb9f1da26c0208b4e5e84d0890860ec54b3a6
gregoryalbertt <gregoryalbertt@gmail.com>
2017-03-26 21:16:43 -0300

    database created

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, af: AngularFire) {
+    this.itens = af.database.list('/itens');
   }
+
+  itens: FirebaseListObservable<any>;
 
 }

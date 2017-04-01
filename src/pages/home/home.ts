af2009eff4a2f2359c4ffd8d53477a539a5725d0
gregoryalbertt <gregoryalbertt@gmail.com>
2017-04-01 16:02:36 -0300

    Creating objects and pushing to firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
@@ -10,10 +10,41 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, af: AngularFire) {
+  constructor(public navCtrl: NavController, af: AngularFire, public alertCtrl: AlertController) {
     this.itens = af.database.list('/itens');
+    
   }
 
   itens: FirebaseListObservable<any>;
+
+  addItem(){
+    let prompt = this.alertCtrl.create({
+      title: 'Item name',
+      message: 'Enter Item description',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+      },
+      ],
+      buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Save',
+        handler: data => {
+          this.itens.push({
+            title: data.title
+          });
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
 
 }
